8fadbdd38087dfe4627b1097a9e2a9b90587cfba
Tuan Anh <tuan73176@gmail.com>
2025-04-30 15:25:30 +0700

    pdx223: Rebrand from pdx225
    - Remove all lights service references and move it to common, since all Sony SM8450 have a LED.
    - Nuke all audio configurations, we already copied it in common.
    
    Change-Id: I0b71604e919e53ddf9ed1e1d9023a5c7dc2c5151

--- lineage_pdx223.mk
+++ lineage_pdx223.mk
@@ -8,24 +8,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from pdx225 device
-$(call inherit-product, device/sony/pdx225/device.mk)
+# Inherit from pdx223 device
+$(call inherit-product, device/sony/pdx223/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_pdx225
-PRODUCT_DEVICE := pdx225
+PRODUCT_NAME := lineage_pdx223
+PRODUCT_DEVICE := pdx223
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
-PRODUCT_MODEL := XQ-CC72
+PRODUCT_MODEL := XQ-CT72
 
 PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="XQ-CC72-user 14 65.2.A.2.224 065002A002022400521077690 release-keys" \
-    BuildFingerprint=Sony/XQ-CC72/XQ-CC72:14/65.2.A.2.224/065002A002022400521077690:user/release-keys \
-    DeviceName=XQ-CC72 \
-    DeviceProduct=XQ-CC72 \
-    SystemDevice=XQ-CC72 \
-    SystemName=XQ-CC72
+    BuildDesc="XQ-CT72-user 14 64.2.A.2.244 064002A002024400519766968 release-keys" \
+    BuildFingerprint=Sony/XQ-CT72/XQ-CT72:14/64.2.A.2.244/064002A002024400519766968:user/release-keys \
+    DeviceName=XQ-CT72 \
+    DeviceProduct=XQ-CT72 \
+    SystemDevice=XQ-CT72 \
+    SystemName=XQ-CT72
