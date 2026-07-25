7688b0be0948b6955130df2badc54be2009bc9dd
Tuan Anh <tuan73176@gmail.com>
2026-07-25 17:40:28 +0000

    pdx223: Bump manifest level to 7
    Change-Id: I2ec7ef61ca3a812e0f10041ff79d950c5418c99f

--- extract-files.py
+++ extract-files.py
@@ -32,6 +32,8 @@ blob_fixups: blob_fixups_user_type = {
     ('odm/etc/customization/XQ-CT72_RU/config.prop', 'odm/etc/customization/XQ-CT72/config.prop', 'odm/etc/customization/XQ-CT62/config.prop',
      'odm/etc/customization/XQ-CT54_EEA/config.prop', 'odm/etc/customization/XQ-CT54/config.prop', 'odm/etc/customization/XQ-CT44/config.prop'): blob_fixup().
         regex_replace('vendor', 'odm'),
+    'vendor/etc/vintf/manifest/vendor.somc.hardware.camera.provider.manifest.xml': blob_fixup()
+        .regex_replace('target-level="6"','target-level="7"'),
     ('vendor/lib64/vendor.somc.camera.device@3.2-impl.so', 'vendor/lib64/vendor.somc.camera.device@3.3-impl.so',
      'vendor/lib64/vendor.somc.camera.device@3.4-impl.so', 'vendor/lib64/vendor.somc.camera.device@3.5-impl.so',
      'vendor/bin/hw/vendor.somc.hardware.camera.provider@1.0-service'): blob_fixup()
